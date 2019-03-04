b40159a6d34abbb7af8883437ff044449ef18e64
Jason Wu <zwu00@stuy.edu>
2019-03-03 20:19:46 -0500

    Added UserOfSpeakers.java

---
+++ abstractMethodViaInheritance/UserOfSpeakers.java
@@ -0,0 +1,9 @@
+public class UserOfSpeakers {
+  public static void main(String[] commandLine) {
+    System.out.println("test");
+  }
+  public String animalCall() {
+    Animal pupper = new Retriever();
+    return pupper.speak();
+  }
+}
